61116f71686caa003c6cc989d197ca476ee6a6a9
Julian Churchill <chewielouie03@gmail.com>
2020-05-23 21:12:26 +0100

    Added IIS hosting config with Kestrel

--- GoBoard/Program.cs
+++ GoBoard/Program.cs
@@ -1,17 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace GoBoard
 {
     public class Program
     {
         public static void Main(string[] args)
+        {
+            IISHost();
+            // SelfHost(args);
+        }
+
+        private static void IISHost()
+        {
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseContentRoot(Directory.GetCurrentDirectory())
+              .UseIISIntegration()
+              .UseStartup<Startup>()
+              .Build();
+            host.Run();
+        }
+
+        private static void SelfHost(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
