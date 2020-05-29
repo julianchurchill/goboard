294f0ab2d1030c04ac50c2b166c274b7b0c080d6
Julian Churchill <chewielouie03@gmail.com>
2020-05-30 00:06:29 +0100

    Added canvas over board to draw stones

--- GoBoard/ClientApp/src/app/goban/goban.component.ts
+++ GoBoard/ClientApp/src/app/goban/goban.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+
 import { StoneModel } from '../models/stone.model';
 import { GobanStoreService } from '../services/goban-store/goban-store.service';
 
@@ -7,25 +8,76 @@ import { GobanStoreService } from '../services/goban-store/goban-store.service';
     templateUrl: './goban.component.html',
     styleUrls: ['./goban.component.css']
 })
-export class GobanComponent {
+export class GobanComponent implements AfterViewInit {
+    @ViewChild('canvas', { static: false })
+    public canvas: ElementRef;
+
+    private cx: CanvasRenderingContext2D;
+    private width = 500;    // TODO: Derive this from the div size
+    private height = 500;   // TODO: Derive this from the div size
+
     constructor(
        private gobanStore: GobanStoreService
     ) {
     }
+
+    public ngAfterViewInit(): void {
+      const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+      this.cx = canvasEl.getContext('2d');
+  
+      canvasEl.width = this.width;
+      canvasEl.height = this.height;
+  
+      this.cx.lineWidth = 3;
+      this.cx.lineCap = 'round';
+      this.cx.strokeStyle = '#000';
+  
+      this.drawStones(canvasEl);
+    }
+
+    private drawStones(canvasEl: HTMLCanvasElement): void {
+        this.gobanStore.stones$.subscribe(stones => {
+            stones.forEach(stone => {
+                this.drawStone(stone);
+            });
+        });
+    }
+
+    private drawStone(stone: StoneModel): void {
+        if (!this.cx) { return; }
+
+        this.cx.beginPath();
     
-    public onClick(event: MouseEvent) {
+        const canvasX = this.findCanvasX(stone.x);
+        const canvasY = this.findCanvasY(stone.y);
+        this.cx.moveTo(canvasX, canvasY);
+        this.cx.lineTo(canvasX + 3, canvasY + 3);
+        this.cx.stroke();
+    }
+    
+    private findCanvasX(boardX: number): number {
+        // TODO
+        return boardX;
+    }
+    
+    private findCanvasY(boardY: number): number {
+        // TODO
+        return boardY;
+    }
+
+    public onClick(event: MouseEvent): void {
         const x = this.findBoardX(event.offsetX);
         const y = this.findBoardY(event.offsetY);
         this.gobanStore.addStone(new StoneModel(x, y));
     }
-    
-    private findBoardX(screenX: number): number {
+
+    private findBoardX(canvasX: number): number {
         // TODO
-        return screenX;
+        return canvasX;
     }
     
-    private findBoardY(screenY: number): number {
+    private findBoardY(canvasY: number): number {
         // TODO
-        return screenY;
+        return canvasY;
     }
 }
