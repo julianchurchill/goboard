1ccfc3991f0c8f0f6102aeffd6b8560400c47463
Julian Churchill <chewielouie03@gmail.com>
2020-05-29 21:25:00 +0100

    Added printed coordinates when clicking to represent stones

--- GoBoard/ClientApp/src/app/goban/goban.component.ts
+++ GoBoard/ClientApp/src/app/goban/goban.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+
+class Stone {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
 @Component({
   selector: 'app-goban-component',
@@ -6,9 +17,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./goban.component.css']
 })
 export class GobanComponent {
-  onClick(event: MouseEvent) {
+  private stones: Stone[] = [];
+  private $stones = new Subject<Stone[]>();
+  public stones$ = this.$stones.asObservable();
+
+  public onClick(event: MouseEvent) {
     const x = event.offsetX;
     const y = event.offsetY;
     console.log(`Clicked at ${x}, ${y}`);
+    this.stones = [...this.stones, new Stone(x, y)];
+    this.$stones.next(this.stones);
   }
 }
